76dcbabf0080ed2244dc3bb8e281da3ec4e38ed2
EC2 Default User <ec2-user@ip-172-31-37-16.us-east-2.compute.internal>
2021-09-15 23:16:48 +0000

    implemented starts_with_consonant passed all tests

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -58,7 +58,7 @@ def hello(name)
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return !s.empty? && s.downcase[0] =~ /[a-z]/i && !s.downcase.start_with?("a", "e", "i", "o", "u")
 end
 
 def binary_multiple_of_4? s
